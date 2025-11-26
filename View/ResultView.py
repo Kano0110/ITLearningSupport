2a1b0f7f05cbbed53db0ebd79994294883141673
takateka <nkc20236140@st.denpa.jp>
2025-11-26 10:01:39 +0900

    実験場でのResultを反映

--- View/ResultView.py
+++ View/ResultView.py
@@ -1,47 +1,91 @@
 # View/ResultView.py
 import tkinter as tk
 from tkinter import ttk, messagebox
+import tkinter.font as tkfont
+import math
 
 class ResultView:
-
-    """wordEntryから変えていないので動かさないで"""
 
     def __init__(self, root: tk.Tk, controller):
         self.root = root
         self.controller = controller
-        # ここでは frame を持たせて pack/forget をコントローラから呼べるようにする
         self.style = ttk.Style()
         self.frame = ttk.Frame(self.root)
+        self.scrollbar_frame = tk.Frame(root)
 
-        self.entry_Name = tk.Text(self.frame, width=40, height=1)
-        self.entry_Yomi = tk.Text(self.frame, width=40,height=1)
-        self.entry_Kai = tk.Text(self.frame, width=40, height=10)
-        self.cb_Category = ttk.Combobox(self.frame, values=[], width=17)
-        self.cb_Tag = ttk.Combobox(self.frame, values=[], width=17)
+        ZanteiCategory = "ネットワーク"
+        ZanteiTag = "絞り込みなし"
+        ZanteiMondaisuu = 10
+        ZanteiSeikai = 8
+        ZanteiPercent = 1500
+        ZanteiWrong = ["DNS","HTTP/HTTPS"]
+        
         self._build_ui()
 #cd_Bunya
 
     def _build_ui(self):
         self.root.title("")
 
-        self.style.configure("My.TButton", foreground="#ff0000")
+        """正解数と正解率"""
+        QuestCount = string_num = str(self.ZanteiMondaisuu)
+        CorrectCount = string_num = str(self.ZanteiSeikai)
 
-        ttk.Label(self.frame, text='単語名：').place(x=60, y=30)
-        self.entry_Name.place(x=150, y=30)
 
-        ttk.Label(self.frame, text='ふりがな：').place(x=59, y=60)
-        self.entry_Yomi.place(x=150, y=60)
+        ZanteiPercent = (self.ZanteiSeikai / self.ZanteiMondaisuu) * 100
+        ZanteiPercent = math.floor(ZanteiPercent)
+        CorrectPerc = string_num = str(ZanteiPercent)
 
-        ttk.Label(self.frame, text='解説：').place(x=65, y=120)
-        self.entry_Kai.place(x=150, y=90)
-        ttk.Label(self.frame, text='カテゴリ').place(x=70, y=260)
-        self.cb_Category.place(x=120, y=260)
-        ttk.Label(self.frame, text='タグ').place(x=315, y=260)
-        self.cb_Tag.place(x=350, y=260)
-        ttk.Button(self.frame, text='単語一覧に戻る', command=lambda: self.controller.create_close_window()).place(x=40, y=340)
-        ttk.Button(self.frame, text='リセット',style="My.TButton", command=lambda: self.controller.create_reset_window()).place(x=490, y=40)
-        ttk.Button(self.frame, text='作成', command=lambda: self.controller.get_id_pass()).place(x=490, y=340)
+        labelCorrect = ttk.Label(self.root, text= QuestCount + "問中、" + CorrectCount + "問正解！",font=self.Correct_font) 
+        labelCorrect.place(x=60,y=40)
 
+        labelPercent = ttk.Label(self.root, text= "正解率：" + CorrectPerc + "%",font=self.Correct_font) 
+        labelPercent.place(x=370,y=38)
+
+        result_font = tkfont.Font(family="Helvetica", size=20, weight="bold")
+        my_font = tkfont.Font(family="Helvetica", size=14, weight="bold")
+        Wrong_font = tkfont.Font(family="Helvetica", size=10, weight="bold")
+        Correct_font = tkfont.Font(family="Helvetica", size=15, weight="bold")
+
+        """解答結果の部品"""
+        labelframe = tk.LabelFrame(self.root, text="絞り込み条件",labelanchor="n",width=300,height=100,font=my_font)
+        labelframe.place(x=60,y=100)
+
+        label_Category = ttk.Label(labelframe, text='カテゴリ：') 
+        label_Category.grid(row=0, column=0, padx=10, pady=5)
+        label_CategoryName = tk.Label(labelframe, text=self.ZanteiCategory, bg = "#ffffff")
+        label_CategoryName.grid(row=0, column=1, padx=10, pady=5)
+
+        label_Tag = ttk.Label(labelframe, text='タグ：')
+        label_Tag.grid(row=1, column=0, padx=10, pady=5)
+        label_TagName = tk.Label(labelframe, text=self.ZanteiTag, bg = "#ffffff")
+        label_TagName.grid(row=1, column=1, padx=10, pady=5)
+
+
+        """各種ボタン"""
+        button_list = ttk.Button(self.root,text = '単語一覧に戻る')
+        button_question = ttk.Button(self.root,text = '出題形式選択へ戻る')
+        button_again = ttk.Button(self.root,text = '同じ条件でもう一度解く')
+        button_list.place(x=70, y=300) #
+        button_question.place(x=70, y=350) #
+        button_again.place(x=400, y=350) #
+
+        """間違えた問題周辺の部品"""
+        labelWrong = ttk.Label(self.root, text="間違えた問題",font=Wrong_font) 
+        listboxWrong = tk.Listbox(self.scrollbar_frame)
+
+        labelWrong.place(x=380, y=100)
+        self.scrollbar_frame.place(x=380, y=120)
+
+        for i in self.ZanteiWrong:
+            listboxWrong.insert(tk.END, i)
+        listboxWrong.pack(side=tk.LEFT)
+
+        scroll_bar =tk.Scrollbar(self.scrollbar_frame, command=listboxWrong.yview)
+        scroll_bar.pack(side=tk.RIGHT, fill=tk.Y)
+        listboxWrong.config(yscrollcommand=scroll_bar.set)
+
+
+    
     def show(self):
         """この View を表示する（controller.show から呼ばれる）。"""
         self.frame.pack(expand=True, fill='both')
